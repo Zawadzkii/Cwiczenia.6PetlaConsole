ac3f56e3f70200952fd809269a89497bd937b323
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-02 20:27:21 +0200

    Add project files.

---
+++ Cwiczenia.6PetlaConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+	static double HarmonicSum(int n)
+	{
+		double total = 0.0;
+		for (int i = 1; i <= n; i++)
+		{
+			total += 1.0 / i;
+		}
+		return total;
+	}
+
+	static void Main(string[] args)
+	{
+		for (int i = 1; i <= 20; i++)
+		{
+			double result = HarmonicSum(i);
+			Console.WriteLine($"Suma dla {i} elementów: {result}");
+		}
+	}
+}
